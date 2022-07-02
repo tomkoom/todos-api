fa1f95726350696f74f93b5d4b84301ea1ed3837
tomkoom <tomash.sugint@gmail.com>
2022-07-02 22:11:18 +0300

    feat: add todos.json

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
+// import todos from ('./todos.json') // from ES6
+
 const http = require("http");
 const port = process.env.PORT || 5000;
+const todos = require("./todos.json");
 
-const server = http.createServer((request, responce) => {
-	responce.writeHead(200, {
-		"content-type": "text/plain",
-	});
-	responce.end("Hello");
+const server = http.createServer((req, res) => {
+	if (req.url === "/api/v1/todos" && req.method === "GET") {
+		res.writeHead(200, {
+			"content-type": "application/json",
+		});
+		res.end(JSON.stringify(todos));
+	}
 });
+
+// const server = http.createServer((request, responce) => {
+// 	responce.writeHead(200, {
+// 		"content-type": "text/plain",
+// 	});
+// 	responce.end("Hello");
+// });
 
 server.listen(port, () => {
 	console.log("Server is ready and running on Port ", port);
 });
 
-server.on("error", (error) => {
-	if ((error.code = "EADRINUSE")) {
+server.on("error", (err) => {
+	if ((err.code = "EADRINUSE")) {
 		console.log("Port already in use");
 	}
 });
